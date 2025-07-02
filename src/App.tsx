c22abe6b380e22bfe39763860accd28fc931bbae
Wesley Lima <contato.wezzlim@outlook.com>
2025-07-02 10:25:21 -0300

    :sparkles: CRUD do Recurso Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
+import ListaCategorias from "./components/categorias/listarcategorias/ListaCategorias";
+import FormCategoria from "./components/categorias/formcategoria/FormCategoria";
+import DeleteCategoria from "./components/categorias/deletecategoria/DeleteCategoria";
 
 function App() {
 
@@ -14,6 +17,10 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} /> 
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} /> 
+              <Route path="/deletarcategoria/:id" element={<DeleteCategoria />} />
             </Routes>
           </div>
         </div>
