b98621e13b9ff841364280a327b9041843abc460
Wesley Lima <contato.wezzlim@outlook.com>
2025-07-02 09:52:39 -0300

    Construção Componentes Home/Navbar/Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,90 @@
+import { MagnifyingGlassIcon, ShoppingCartIcon, UserIcon } from "@phosphor-icons/react"
+import { useState, type ChangeEvent, type FormEvent } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
+
+function Navbar() {
+
+	const navigate = useNavigate()
+
+	const [nome, setNome] = useState<string>("")
+
+	function handleBuscarProdutos(e: ChangeEvent<HTMLInputElement>){
+		setNome(e.target.value)
+	}
+
+	function buscarProdutos(e: FormEvent<HTMLFormElement>){
+		e.preventDefault()
+		navigate(`/consultarnome/${nome}`)
+		setNome('')
+	}
+	
+	return (
+		<>
+			<div className="flex justify-center w-full py-4 text-white bg-indigo-900">
+				<div className="container flex items-center justify-between mx-4 text-lg">
+					<Link to="/home">
+						<img
+							src="https://i.imgur.com/jOpl9Qi.png"
+							alt="Logo"
+							className="w-60"
+						/>
+					</Link>
+
+					<div className="relative flex items-center justify-center w-2/5 text-black">
+						<form 
+							className="flex items-center justify-center w-full"
+							onSubmit={buscarProdutos}
+						>
+							<input
+								className="w-10/12 px-4 py-4 bg-white rounded-lg h-9 focus:outline-none"
+								type="search"
+								placeholder="Pesquisar produto pelo nome"
+								id="nome"
+								name="nome"
+								required
+								value={nome}
+								onChange={(e: ChangeEvent<HTMLInputElement>) => handleBuscarProdutos(e)}
+							/>
+							<button
+								type="submit"
+								className="h-9 w-9 p-2.5 ms-2 text-sm font-medium text-white bg-teal-500 hover:bg-teal-900 rounded-lg border border-teal-700"
+							>
+								<MagnifyingGlassIcon
+									size={14}
+									weight="bold"
+								/>
+							</button>
+						</form>
+					</div>
+
+					<div className="flex items-center gap-4 py-4">
+						<Link
+							to="/categorias"
+							className="hover:underline"
+						>
+							Categorias
+						</Link>
+						<Link
+							to="/cadastrarcategoria"
+							className="hover:underline"
+						>
+							Cadastrar Categoria
+						</Link>
+							<UserIcon
+								size={32}
+								weight="bold"
+							/>
+
+							<ShoppingCartIcon
+								size={32}
+								weight="bold"
+							/>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,58 @@
+﻿function Home() {
+    return (
+        
+            <div className="
+                bg-cyan-400
+                flex 
+                justify-center
+                py-32
+                ">
+                <div className='
+                    container 
+                    grid 
+                    grid-cols-1 md:grid-cols-2 
+                    text-white
+                    gap-8 md:gap-0
+                    px-4
+                    '>
+                    <div className="
+                        flex 
+                        flex-col 
+                        gap-4 
+                        items-center 
+                        justify-center 
+                        py-4
+                        text-center
+                        ">
+                        <h2 className='
+                            text-3xl md:text-5xl 
+                            font-bold
+                            text-black
+                            '>
+                            Seja bem vindo!
+                        </h2>
+                        <p className='text-base md:text-xl text-black'>Aqui você encontra Medicamentos e Cosméticos!</p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                            >
+                                Cadastrar Produto
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center items-center">
+                        <img
+                            src="https://i.imgur.com/ETRgx50.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3 max-w-xs md:max-w-md lg:max-w-lg'
+                        />
+                    </div>
+                </div>
+            </div>
+        
+    )
+}
+
+export default Home
